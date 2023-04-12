41875c86f80077ecd7d9fbb747524d388d0b7d85
NikitaSuvorov1 <86976882+NikitaSuvorov1@users.noreply.github.com>
2023-04-12 20:08:29 +0300

    fix

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const app = express()
 
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
+        if (!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads');
+        }
         cb(null, 'uploads');
     },
     filename: (_, file, cb) => {
