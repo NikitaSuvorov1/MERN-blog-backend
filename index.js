dc54ab2f175c742c8e18c5208583b5b650f0ce1e
NikitaSuvorov1 <kipish1033@gmail.com>
2023-04-12 08:32:09 +0300

    fix

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ import * as UserController from './controllers/userController.js'
 import * as PostController from './controllers/PostController.js'
 import * as CommentController from './controllers/CommentController.js'
 
-        mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.k09ry4k.mongodb.net/blog?retryWrites=true&w=majority').then(() => console.log("DB ok")).catch((error) => console.log(error))
+
+///'mongodb+srv://admin:wwwwww@cluster0.k09ry4k.mongodb.net/blog?retryWrites=true&w=majority'
+        mongoose.connect(process.env.MONGODB_URI).then(() => console.log("DB ok")).catch((error) => console.log(error))
 const app = express()
 
 const storage = multer.diskStorage({
