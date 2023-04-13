13547abf948c94b5ad0d22fe598aace01276b98c
NikitaSuvorov1 <86976882+NikitaSuvorov1@users.noreply.github.com>
2023-04-13 16:13:55 +0300

    fix

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-app.use(cors())
+app.use(cors({origin: true, credentials: true}));
 app.use(express.json())
 app.use('/uploads', express.static('uploads'));
 
